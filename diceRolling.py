0b912e766f4fbb7ab9253d230b8b6e152aab25d3
Ana Leite <a.claraaleite@gmail.com>
2026-04-19 08:29:58 -0300

    Update diceRolling.py

--- diceRolling.py
+++ diceRolling.py
@@ -4,18 +4,18 @@
 - everytime the user puts 'y' the game keeps going, however, if they insert 'n' then it stops
 - at each roll of the dice, it sorts two numbers: (x,y)
 '''
-import random
-a = 0
-while a==0: 
+import random 
+a = 0 
+while a==0: #the while is useful so the program runs over and over again until the user doesn't want to play anymore. 
     answer = str(input('Roll the dice? (y/n): '))
-    if answer.lower() == 'n':
+    if answer.lower() == 'n': 
         print('Thanks for playing!')
-        break
+        break #as previously said, the game will keep running until the player types 'n'
     elif answer.lower() == 'y':
-        dice = [1, 2, 3, 4, 5, 6]
-        firstNumber = random.choice(dice)
-        secondNumber = random.choice(dice)
-        print(f'something, ({firstNumber}, {secondNumber})')
+        dice = [1, 3, 5] #two lists to avoid the same number falling twice
+        dice2 = [2, 4, 6]
+        firstNumber = random.choice(dice) 
+        secondNumber = random.choice(dice2)
+        print(f'({firstNumber}, {secondNumber})')
     else: 
         print('invalid input, please try again.')
-    
